bc9e4922c6f94795edab96300bec2468ab1a2c6f
chandra shekar kanukuntla <36542310+chandrashekar1@users.noreply.github.com>
2020-10-01 06:20:26 +0530

    polynomials
    For this purpose, we turn instances

---
+++ polynomials.py
@@ -0,0 +1,20 @@
+class Polynomial:
+    
+    def __init__(self, *coefficients):
+        """ input: coefficients are in the form a_n, ...a_1, a_0 
+        """
+        self.coefficients = list(coefficients) # tuple is turned into a list
+     
+    def __repr__(self):
+        """
+        method to return the canonical string representation 
+        of a polynomial.
+   
+        """
+        return "Polynomial" + str(self.coefficients)
+            
+    def __call__(self, x):    
+        res = 0
+        for index, coeff in enumerate(self.coefficients[::-1]):
+            res += coeff * x** index
+        return res 
